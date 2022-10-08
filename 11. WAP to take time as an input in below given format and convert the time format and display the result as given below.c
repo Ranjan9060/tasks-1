15834247535df3eb1e73aa16efe245d67738d422
Ranjan Yadav <108995846+Ranjan9060@users.noreply.github.com>
2022-10-08 03:27:26 -0400

    Add files via upload

---
+++ 11. WAP to take time as an input in below given format and convert the time format and display the result as given below.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int hh,mm;
+
+    printf("Enter a Hour and Minute :");
+
+    scanf("%d:%d",&hh,&mm);
+
+    printf("%d hour and %d Minute",hh,mm);
+
+    return 0;
+}
