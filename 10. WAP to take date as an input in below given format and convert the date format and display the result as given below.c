f60cfa97a66c598bc8fb8b41c194c3d821d2e51d
Ranjan Yadav <108995846+Ranjan9060@users.noreply.github.com>
2022-10-08 03:22:44 -0400

    Add files via upload

---
+++ 10. WAP to take date as an input in below given format and convert the date format and display the result as given below.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int dd,mm,yyy;
+
+    printf("Enter day,month and year :");
+
+    scanf("%d/%d/%d",&dd,&mm,&yyy);
+
+    printf("Day – %d , Month – %d, Year – %d ",dd,mm,yyy);
+    
+    return 0;
+}
